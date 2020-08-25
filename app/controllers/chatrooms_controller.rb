8a89567139015e4aa97ffdb698ec413492202b3d
Maroussia Arnault <maroussia.arnault@gmail.com>
2020-08-25 12:56:00 +0200

    Added messages controller

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,5 +1,10 @@
 class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
+    @message = @chatroom.message
+  end
+
+  def messsage_params
+    params.require(:message).permit(:content)
   end
 end
---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,21 @@
+class MessagesController < ApplicationController
+
+  def create
+    @chatroom = Chatroom.find(params[:chatroom_id])
+    @message = Message.new(message_params)
+    @message.chatroom = @chatroom
+    @message.user = current_user
+    if @message.save
+      redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
+    else
+      render "chatrooms/show"
+    end
+  end
+
+
+  private
+
+  def messsage_params
+    params.require(:message).permit(:content)
+  end
+end
