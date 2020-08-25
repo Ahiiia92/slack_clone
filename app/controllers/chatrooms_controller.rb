ed625eddf0e67ba820543def8fbb68adf91ca89b
Maroussia Arnault <maroussia.arnault@gmail.com>
2020-08-25 15:43:20 +0200

    views, messages and chatroom linked

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,7 +1,7 @@
 class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
-    @message = @chatroom.message
+    @message = Message.new
   end
 
   def messsage_params
